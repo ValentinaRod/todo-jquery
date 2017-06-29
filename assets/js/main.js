eb285e2f860554ada63e38a22118e7b4b6bd73dd
Valentina1425 <valentina.rodriguez1425@gmail.com>
2017-06-29 10:59:14 -0400

    añado archivos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,18 @@
 //crear elementos
 $(document).ready(inicio);
-	function inicio(){
-			//crear elemento input
-		var text= $('#val').val('Ingresar Tarea()');
-			$('#List').append('<li>'+ text + '<input type="checkbox" id="test5" /><label for="test5">Red</input>' + '<a class="waves-effect waves-light btn">remove</a>' +'</li>');
-};
+(function($){
+    $('#myform').submit(function(e){
+        var val = $(this).find('#in').val();
+        $('ul.list').append('<li>' + val + '</li>');
+        e.preventDefault();
+    });
+})(jQuery);
+
+/*checkbox*/
+
+$('#isAgeSelected').click(function() {
+    $("#txtAge").toggle(this.checked);
+});
 	
 
 
