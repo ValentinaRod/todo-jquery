476c96a412994aad11d6299f82145c26c9daf92d
ValentinaRod <v.rodriguez1425@gmail.com>
2017-06-29 02:10:18 -0400

    atchivos

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+//crear elementos
+$(document).ready(inicio);
+	function inicio(){
+			//crear elemento input
+		var text= $('#val').val('Ingresar Tarea()');
+			$('#List').append('<li>'+ text + '<input type="checkbox" id="test5" /><label for="test5">Red</input>' + '<a class="waves-effect waves-light btn">remove</a>' +'</li>');
+};
+	
+
+
+
